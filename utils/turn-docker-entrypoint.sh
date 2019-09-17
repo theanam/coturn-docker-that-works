1f2cf0b203d6c16e5591489b272695aecc084b76
Anam Ahmed <anam.ahmed.a@gmail.com>
2019-09-17 13:57:12 +0600

    single entrypoint file

--- utils/turn-docker-entrypoint.sh
+++ utils/turn-docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Generate Self Signed SSL certificate
+if [ ! -f /opt/turn.pem ]; then
+    echo "GENERATING SELF SIGNED SSL CERTIFICATES..."
+    openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+        -keyout turn.key -out turn.crt -subj /CN=${DOMAIN}     
+fi
+# Generate TURN config
 echo "Creating Fresh TURN config file...."
 CONF=/etc/coturn/turnserver.conf
 echo "Using conf file: ${CONF}" &&\
